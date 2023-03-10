1790a819365b5f63c612846e8469aa4a620caebd
bokigolic <bokigolic32@gmail.com>
2023-03-10 11:37:08 -0500

    work on note taker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Test from './component/Test';
+import NoteTaker from './component/NoteTaker';
 
 function App() {
   return (
     <div className="App">
       <>
+
+
+        <NoteTaker />
         <Test />
+
       </>
     </div>
   );
