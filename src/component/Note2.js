63519c254ba2fe5d9c9b68471983bbc2b2422236
bokigolic <bokigolic32@gmail.com>
2023-03-11 23:07:29 -0500

    add onchange funcionality

--- src/component/Note2.js
+++ src/component/Note2.js
@@ -1,15 +1,49 @@
+import { useState } from "react"
+
 const Note2 = () => {
+
+  const handleChange = () => {
+
+  }
+
+  const [zaInput, setZaInput] = useState({
+    ime: "",
+    prezime: "",
+    email: "",
+    telefon: ""
+  })
   return (
     <div className="note-dva">
       <h2>Note 2</h2>
       <label>Ime: </label>
-      <input /> <br />
+      <input
+        type="text"
+        name="ime"
+        value={zaInput.ime}
+        onChange={handleChange}
+      /> <br />
       <label>Prezime: </label>
-      <input /> <br />
+      <input
+        type="text"
+        name="prezime"
+        value={zaInput.prezime}
+        onChange={handleChange}
+      /> <br />
       <label>Email: </label>
-      <input /> <br />
+      <input
+        type="text"
+        name="email"
+        value={zaInput.email}
+        onChange={handleChange}
+      /> <br />
       <label>Broj Telefona: </label>
-      <input /> <br />
+      <input
+        type="text"
+        name="telefon"
+        value={zaInput.telefon}
+        onChange={handleChange}
+      /> <br />
+      <button type="button">Add</button>
     </div>
   )
 }
