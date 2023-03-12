6c88d3b122f953936c6a908ec629507c52df1e33
bokigolic <bokigolic32@gmail.com>
2023-03-11 22:49:58 -0500

    create and delete in progress

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Test from './component/Test';
 import NoteTaker from './component/NoteTaker';
 import Test2 from './component/Test2';
+import Note2 from './component/Note2';
 
 
 function App() {
   return (
     <div className="App">
       <>
+        <Note2 />
         <Test2 />
         <NoteTaker />
         <Test />
---
+++ src/component/Note2.js
@@ -0,0 +1,16 @@
+const Note2 = () => {
+  return (
+    <div className="note-dva">
+      <h2>Note 2</h2>
+      <label>Ime: </label>
+      <input /> <br />
+      <label>Prezime: </label>
+      <input /> <br />
+      <label>Email: </label>
+      <input /> <br />
+      <label>Broj Telefona: </label>
+      <input /> <br />
+    </div>
+  )
+}
+export default Note2
