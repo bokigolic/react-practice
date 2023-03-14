a9aee7046759caa9fdc41beccbd4b3f25081e587
bokigolic <bokigolic32@gmail.com>
2023-03-13 23:05:35 -0400

    registration form done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Test from './component/Test';
 import NoteTaker from './component/NoteTaker';
 import Test2 from './component/Test2';
 import Note2 from './component/Note2';
+import RegistrationForm from './component/RegistrationForm';
 
 
 function App() {
   return (
     <div className="App">
       <>
+        <RegistrationForm />
         <Note2 />
         <Test2 />
         <NoteTaker />
