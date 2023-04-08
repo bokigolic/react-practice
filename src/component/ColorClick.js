e318667f3eb8637bbc72afc5155383fa69eb307b
bokigolic <bokigolic32@gmail.com>
2023-04-08 00:27:17 -0400

    update

---
+++ src/component/ColorClick.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ColorClick = () => {
+  return (
+    <div className="forma">
+      <h3 className='registration'>sss</h3>
+    </div>
+  )
+}
+
+export default ColorClick
