c462ad9117ed53d2c6eca300a6c5e7ff453522d3
bokigolic <bokigolic32@gmail.com>
2023-04-09 23:00:43 -0400

    update counetr

--- src/component/ColorClick.js
+++ src/component/ColorClick.js
@@ -9,7 +9,7 @@ const ColorClick = () => {
 
   return (
     <div className="forma">
-      <h3 className='registration'>Click to change color:  {color}</h3>
+      <h3 className='registration' style={{color: color}}>Click to change color: {color}</h3>
       <button type='button' onClick={() => handleClick("red")} style={{color: "red"}}>red</button>
       <button type='button' onClick={() => handleClick("black")} style={{color: "black"}}>black</button>
       <button type='button' onClick={() => handleClick("blue")} style={{color: "blue"}}>blue</button>
